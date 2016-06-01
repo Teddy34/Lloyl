874ab73e1727baeb70f8bb9d1563c295567f3616
JSicart <jsicart@intercalleurope.com>
2016-06-01 17:21:23 +0200

    added picture, paging and initial sort

--- src/app.js
+++ src/app.js
@@ -5,17 +5,24 @@ import _ from 'lodash';
 
 const format = (num) => num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-fetch("https://crest-tq.eveonline.com/insuranceprices/")
-.then(response => response.json())
-.then(({items}) => _.map(items, ({type, insurance}) => {
-    return {
-        Name:type.name,
-        Id: type.id_str,
-        Payout: format(_.last(insurance).payout),
-        Cost: format(_.last(insurance).cost),
-        Margin: format(_.last(insurance).payout - _.last(insurance).cost)
-    };
-}))
-.then(formatedItemList => {
-    ReactDOM.render(<Table  data={formatedItemList} filterable={['Name']}/>, document.getElementById('app'));
+const iconURL = "https://image.eveonline.com/Type/";
+const getImageUrl = (typeID) => `${iconURL}${typeID}_32.png`;
+
+fetch("https://crest-tq.eveonline.com/insuranceprices/", {
+    headers: {"Accept-Language": "EN-en"}
 })
+.then(response => response.json())
+.then(({items}) => _(items).map(({type, insurance}) => {
+        return {
+            "": <img src={getImageUrl(type.id_str)}/>,
+            Name:type.name,
+            Payout: format(_.last(insurance).payout),
+            Cost: format(_.last(insurance).cost),
+            Margin: format(_.last(insurance).payout - _.last(insurance).cost)
+        };
+    })
+    .sortBy('Name').value()
+)
+.then(formatedItemList => {
+    ReactDOM.render(<Table  data={formatedItemList} filterable={['Name']} itemsPerPage={20}/>, document.getElementById('app'));
+});
