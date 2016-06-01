ed94f7de7da1d86db7c389adf4b26b6750ce71d4
JSicart <jsicart@intercalleurope.com>
2016-06-01 16:23:44 +0200

    simplifly a bit

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ const format = (num) => num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 fetch("https://crest-tq.eveonline.com/insuranceprices/")
 .then(response => response.json())
-.then(response => response.items)
-.then(itemList => _.map(itemList, ({type, insurance}) => {
+.then(({items}) => _.map(items, ({type, insurance}) => {
     return {
         Name:type.name,
         Id: type.id_str,
@@ -17,7 +16,6 @@ fetch("https://crest-tq.eveonline.com/insuranceprices/")
         Margin: format(_.last(insurance).payout - _.last(insurance).cost)
     };
 }))
-
 .then(formatedItemList => {
     ReactDOM.render(<Table  data={formatedItemList} filterable={['Name']}/>, document.getElementById('app'));
 })
